4bd075269b3ed3a993457a788991d52550d9611b
NicolasPetiot <nicolaspetiot2710@hotmail.fr>
2023-11-22 14:53:21 +0100

    add fetch_protein_data_bank + move static methods outside of PDBfile class

--- _AtomFrame.py
+++ _AtomFrame.py
@@ -2,6 +2,11 @@ from ._parameters import *
 import pandas as pd
 import numpy as np
 
+import sys
+from urllib.request import urlopen
+
+__all__ = ["PDBfile", "atom_position", "atom_mass", "atom_bfactors", "fetch_protein_data_bank", "angles", "dihedral_angles", "theta_angles", "gamma_angles", "chi_angles"]
+
 class PDBfile :
     """
     Iterator object that allow to loop over the frames in a md trajectory in the PDB format.
@@ -71,11 +76,11 @@ class PDBfile :
         atoms = []
         for line in self.file :
             if line[:6] == "ENDMDL":
-                return self._build_df_from_atom_list(atoms)
+                return _build_df_from_atom_list(atoms)
             
             # append atoms with potential new informations
             if line[:6] in {"ATOM  ", "HETATM"}:
-                atoms.append(self._scan_pdb_line(line))
+                atoms.append(_scan_pdb_line(line))
             
         raise StopIteration
     
@@ -93,54 +98,6 @@ class PDBfile :
     #################################
 
     ### Manipulate pdb lines ###
-    @staticmethod
-    def _build_df_from_atom_list(atoms:list):
-        """
-        Returns a pandas.DataFrame that contains the atoms in the input list.
-        """
-        columns=["record_name", "name", "alt", "resn", "chain", "resi", "insertion", "x", "y", "z", "occupancy", "b", "segi", "e", "q", "m"]
-        return pd.DataFrame(atoms, columns=columns)
-
-    @staticmethod
-    def _scan_pdb_line(line:str):
-        """
-        Returns a tuple that contains the (record_name, name, alt, resn, chain, resi, insertion, x, y, z, occupancy, b, segi, elem, charge, mass) 
-        informations about an atom line in the PDB file.
-        """
-        if line.startswith("ATOM"):
-            record_name = "ATOM  "
-        elif line.startswith("HETATM"):
-            record_name = "HETATM"
-        else :
-            raise ValueError("Input 'line' is not associated to an atom in a pdb file.")
-        
-        #atom_id   = line[ 6:11]
-        name      = line[12:16]
-        alt       = line[16:17]
-        resn      = line[17:20]
-        chain     = line[21:22]
-        resi      = int(line[22:26])
-        insertion = line[26:27]
-        x         = float(line[30:38])
-        y         = float(line[38:46])
-        z         = float(line[46:54])
-        try :
-            occupancy = float(line[54:60])
-        except ValueError :
-            occupancy = 1.0
-        try :
-            b         = float(line[60:66])
-        except ValueError :
-            b         = 0.0
-        segi      = line[72:76]
-        elem      = line[76:78]
-        charge    = line[78:79]
-        try :
-            mass      = ATOMIC_MASSES[elem]
-        except KeyError:
-            raise KeyError(f"Unknown element symbol '{elem}'. Please update the ATOMIC_MASSES dictionary in '_parameters.py'.")
-        return record_name, name, alt, resn, chain, resi, insertion, x, y, z, occupancy, b, segi, elem, charge, mass
-    
     def read2df(self):
         """
         Returns a pandas.DataFrame that contains all the atoms fount in the PDB file.
@@ -149,69 +106,8 @@ class PDBfile :
         with open(self.file.name, "r") as file :
             for line in file:
                 if line[:6] in {"ATOM  ", "HETATM"}:
-                    atoms.append(self._scan_pdb_line(line))
-        return self._build_df_from_atom_list(atoms) 
-    
-    @classmethod
-    def df2pdb(cls, df:pd.DataFrame, filename: str, title = "", remark = "") -> None:
-        """
-        Generates a pdb file from an inputed DataFrame.
-        """
-        with open(filename, "w") as file :
-            if title != "":
-                file.write("TITLE     " + title + "\n")
-            if remark != "":
-                for line in remark.splitlines():
-                    file.write("REMARK    " + line + "\n")
-            
-            file.write("MODEL        1\n")
-            id = 0
-            APO = df[df.record_name == "ATOM  "]
-            HET = df[df.record_name == "HETATM"]
-            for _, chain in APO.groupby(["chain"]):
-                for _, atom in chain.iterrows():
-                    id += 1
-                    line = cls._generate_pdb_line(atom, id)
-                    file.write(line)
-                file.write("TER\n")
-            for atom in HET.iterrows():
-                id += 1
-                line = cls._generate_pdb_line(atom, id)
-                file.write(line)
-            file.write("TER\n")
-            file.write("ENDMDL\n")
-
-    @staticmethod
-    def _generate_pdb_line(atom:pd.Series, id:int) -> str:
-        """
-        Generates a pdb line from an input series that contains atom's information.
-        """
-        # extract strings :
-        record_name = atom["record_name"]
-        name        = atom["name"]
-        alt         = atom["alt"]
-        resn        = atom["resn"]
-        chain       = atom["chain"]
-        insertion   = atom["insertion"]
-        segi        = atom["segi"]
-        elem        = atom["e"]
-        charge      = atom["q"]
-
-        # extract float/int
-        id    = f"{id:5d}"
-        resi  = f"{atom.resi:4d}"
-        x     = f"{atom.x:8.3f}"
-        y     = f"{atom.y:8.3f}"
-        z     = f"{atom.z:8.3f}"
-        occup = f"{atom.occupancy:6.2f}"
-        b     = f"{atom.b:6.2f}"
-
-        line = "%s%s %s%s%s %s%s%s   %s%s%s%s%s      %s%s%s\n"%(
-            record_name, id, name, alt, resn, chain, resi, insertion, x, y, z, occup, b, segi, elem, charge
-        )
-        return line
-
-
+                    atoms.append(_scan_pdb_line(line))
+        return _build_df_from_atom_list(atoms) 
     ############################
     
 
@@ -233,7 +129,26 @@ def atom_bfactors(df:pd.DataFrame) -> np.ndarray:
     """
     return df["b"].to_numpy(dtype=float)
 
-    
+### Load from Protein Data Bank ###
+
+def fetch_protein_data_bank(pdb_code:str)->pd.DataFrame:
+    """
+    Returns a pandas.DataFrame associated to the structure loaded from the 'rcsb.org' website.
+    """
+    url = f"https://files.rcsb.org/download/{pdb_code.lower()}.pdb"
+    response = urlopen(url)
+
+    txt = response.read()
+    lines = (txt.decode("utf-8") if sys.version_info[0] >= 3 else txt.decode("ascii")).splitlines()
+
+    atoms = []
+    for line in lines :
+        if line[:6] in {"ATOM  ", "HETATM"}:
+            atoms.append(PDBfile._scan_pdb_line(line))
+
+    return PDBfile._build_df_from_atom_list(atoms)
+
+
 ### Conformational angles computations ###
 
 def angles(atom_position:np.ndarray) -> np.ndarray:
@@ -314,3 +229,108 @@ def chi_angles(df:pd.DataFrame):
             Chis["%d"%i].append(np.nan)
 
     return Chis
+
+### Useful functions that will not be in the __all__ list ###
+
+def _build_df_from_atom_list(atoms:list):
+    """
+    Returns a pandas.DataFrame that contains the atoms in the input list.
+    """
+    columns=["record_name", "name", "alt", "resn", "chain", "resi", "insertion", "x", "y", "z", "occupancy", "b", "segi", "e", "q", "m"]
+    return pd.DataFrame(atoms, columns=columns)
+
+def _scan_pdb_line(line:str):
+    """
+    Returns a tuple that contains the (record_name, name, alt, resn, chain, resi, insertion, x, y, z, occupancy, b, segi, elem, charge, mass) 
+    informations about an atom line in the PDB file.
+    """
+    if line.startswith("ATOM"):
+        record_name = "ATOM  "
+    elif line.startswith("HETATM"):
+        record_name = "HETATM"
+    else :
+        raise ValueError("Input 'line' is not associated to an atom in a pdb file.")
+    
+    #atom_id   = line[ 6:11]
+    name      = line[12:16]
+    alt       = line[16:17]
+    resn      = line[17:20]
+    chain     = line[21:22]
+    resi      = int(line[22:26])
+    insertion = line[26:27]
+    x         = float(line[30:38])
+    y         = float(line[38:46])
+    z         = float(line[46:54])
+    try :
+        occupancy = float(line[54:60])
+    except ValueError :
+        occupancy = 1.0
+    try :
+        b         = float(line[60:66])
+    except ValueError :
+        b         = 0.0
+    segi      = line[72:76]
+    elem      = line[76:78]
+    charge    = line[78:79]
+    try :
+        mass      = ATOMIC_MASSES[elem]
+    except KeyError:
+        raise KeyError(f"Unknown element symbol '{elem}'. Please update the ATOMIC_MASSES dictionary in '_parameters.py'.")
+    return record_name, name, alt, resn, chain, resi, insertion, x, y, z, occupancy, b, segi, elem, charge, mass
+
+def _generate_pdb_line(atom:pd.Series, id:int) -> str:
+    """
+    Generates a pdb line from an input series that contains atom's information.
+    """
+    # extract strings :
+    record_name = atom["record_name"]
+    name        = atom["name"]
+    alt         = atom["alt"]
+    resn        = atom["resn"]
+    chain       = atom["chain"]
+    insertion   = atom["insertion"]
+    segi        = atom["segi"]
+    elem        = atom["e"]
+    charge      = atom["q"]
+
+    # extract float/int
+    id    = f"{id:5d}"
+    resi  = f"{atom.resi:4d}"
+    x     = f"{atom.x:8.3f}"
+    y     = f"{atom.y:8.3f}"
+    z     = f"{atom.z:8.3f}"
+    occup = f"{atom.occupancy:6.2f}"
+    b     = f"{atom.b:6.2f}"
+
+    line = "%s%s %s%s%s %s%s%s   %s%s%s%s%s      %s%s%s\n"%(
+        record_name, id, name, alt, resn, chain, resi, insertion, x, y, z, occup, b, segi, elem, charge
+    )
+    return line
+
+def df2pdb(cls, df:pd.DataFrame, filename: str, title = "", remark = "") -> None:
+    """
+    Generates a pdb file from an inputed DataFrame.
+    """
+    with open(filename, "w") as file :
+        if title != "":
+            file.write("TITLE     " + title + "\n")
+        if remark != "":
+            for line in remark.splitlines():
+                file.write("REMARK    " + line + "\n")
+            
+        file.write("MODEL        1\n")
+        id = 0
+        APO = df[df.record_name == "ATOM  "]
+        HET = df[df.record_name == "HETATM"]
+        for _, chain in APO.groupby(["chain"]):
+            for _, atom in chain.iterrows():
+                id += 1
+                line = cls._generate_pdb_line(atom, id)
+                file.write(line)
+            file.write("TER\n")
+        for atom in HET.iterrows():
+            id += 1
+            line = _generate_pdb_line(atom, id)
+            file.write(line)
+        file.write("TER\n")
+        file.write("ENDMDL\n")
