0b6e612e09d341bae59cd497177d0d55fddd84d3
NicolasPetiot <nicolaspetiot2710@hotmail.fr>
2023-12-12 10:56:55 +0100

    change __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,9 @@ from ._parameters import *
 from ._pdb_file_class import *
 from ._atom_frame_analysis import *
 
+# sub packages :
 from . import NMA
+try :
+    from . import CUTABI
+except ImportError:
+    pass
