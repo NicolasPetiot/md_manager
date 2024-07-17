2c1ee3004cef348987a63a61d27b17474743ba14
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2024-07-17 15:52:23 +0200

    Get ready for release in pipy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file :
 
 setup(
     name="md_manager",
-    version="0.0.2",
+    version="0.0.3",
     description="A python module that allow molecular dynamics data analysis based on pandas DataFrames.",
     long_description=doc,
 
